609677156df12bc8ade6642d4ac7d46f37b5ff75
Blake <blakespyda@hotmail.com>
2021-05-26 12:01:05 +0200

    Blake first Commit

---
+++ crystal puzzle prototype/Assets/Scripts/Shoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    private GameObject lookDirection;
+    private float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookDirection = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * Time.deltaTime * speed);
+    }
+}
