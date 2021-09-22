65a3aedb38f7f2ff483940c0787f592e45a61a9a
Blake <blakespyda@hotmail.com>
2021-09-22 15:05:13 +0200

    Added dashing
    made dashing animations, started dashing logic. dashing works but needs lots of tweaking with animation and logic.

---
+++ Crystal Sword/Assets/Scripts/PlayerDash.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDash : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody2D playerRb;
+    private Vector2 direction;
+    private float dashTime;
+    public float startDashTime;
+    public float dashSpeed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //pulls players direction data
+        direction.x = anim.GetFloat("Horizontal");
+        direction.y = anim.GetFloat("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        Dash();
+    }
+    private void Dash()
+    {
+        //stops player from spamming dash attack
+        if (dashTime <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                anim.SetTrigger("Dashing");
+                playerRb.velocity = direction * dashSpeed;
+                dashTime = startDashTime;
+                Debug.Log("WOOO!");
+            }
+        }
+        else dashTime -= Time.deltaTime;
+    }
+}
