91c3d6cc9d78c44709f29d0077d04cc267be5abc
Blake <blakespyda@hotmail.com>
2021-10-26 20:53:22 +0200

    Fixing LaserBeam logic
    Actually getting somewhere. latest problem is making separate blocks behave independently

--- Crystal Sword/Assets/Scripts/FireLaser.cs
+++ Crystal Sword/Assets/Scripts/FireLaser.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FireLaser : MonoBehaviour
-{
-    private Vector3 bendLaser = new Vector3(0, 0, 90);
+{ 
     private int bounce;
     private LineRenderer lR;
     [SerializeField]
@@ -13,6 +12,7 @@ public class FireLaser : MonoBehaviour
     private LayerMask mirrors;
     public Transform shootPoint1;
     public Transform shootPoint2;
+    public ReflectLaser reflectLaser;
 
 
 
@@ -25,13 +25,50 @@ public class FireLaser : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKey(KeyCode.Z))
         {
             CastRay(transform.position, -transform.up);
         }
     }
 
     private void CastRay(Vector2 position, Vector2 direction)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(position, direction);
+        Debug.DrawRay(position, direction, Color.blue);
+        lR.positionCount = 2;
+        lR.SetPosition(0, transform.position);
+        Vector3 savePos;
+        if (hit.collider != null)
+        {
+            savePos = hit.point;
+            if (hit.collider.name == "Mirror 1")
+            {
+                lR.SetPosition(1, savePos);
+                position = shootPoint1.position;
+                direction = shootPoint1.transform.up;
+                hit = Physics2D.Raycast(position, direction);
+                Debug.DrawRay(position, direction, Color.blue);
+                reflectLaser.HitMirror1(hit.point);
+
+            }
+            if (hit.collider.name == "Mirror 2")
+            {
+                lR.SetPosition(1, savePos);
+                position = shootPoint2.position;
+                direction = shootPoint2.transform.up;
+                hit = Physics2D.Raycast(position, direction);
+                Debug.DrawRay(position, direction, Color.blue);
+                reflectLaser.HitMirror2(hit.point);
+            }
+            if (hit.collider.CompareTag("Button"))
+            {
+                lR.SetPosition(1, savePos);
+                Debug.Log("Win!");
+            }
+        }
+    }
+
+    /*private void CastRay(Vector2 position, Vector2 direction)
     {
 
         RaycastHit2D hit = Physics2D.Raycast(position, direction);
@@ -64,5 +101,5 @@ public class FireLaser : MonoBehaviour
 
 
         }
-    }
+    }*/
 }
---
+++ Crystal Sword/Assets/Scripts/ReflectLaser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectLaser : MonoBehaviour
+{
+    private LineRenderer lR;
+
+    public Transform shootPoint1;
+    public Transform shootPoint2;
+    private void Start()
+    {
+        lR = GetComponent<LineRenderer>();
+        lR.positionCount = 2;
+    }
+
+    public void HitMirror1(Vector3 position)
+    {
+        lR.SetPosition(0, shootPoint1.position);
+        lR.SetPosition(1, position);
+        Debug.Log("Im reflecting!");
+    }
+    public void HitMirror2(Vector3 position)
+    {
+        lR.SetPosition(0, shootPoint2.position);
+        lR.SetPosition(1, position);
+    }
+}
