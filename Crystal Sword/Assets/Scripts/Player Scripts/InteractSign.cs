311f3d2a3054ff5fcae3f944568532403036251a
Blake <blakespyda@hotmail.com>
2021-11-23 14:30:29 +0100

    Imported new sprites, Interact indicator added
    Set up Oldman, and couple of enemy sprites, fixed dialog box problems and added an interact prompt for the player when approaching something interactable.

---
+++ Crystal Sword/Assets/Scripts/Player Scripts/InteractSign.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractSign : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject interact;
+    private bool interacting;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && interacting)
+        {
+            interact.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Interactable"))
+        {
+            interact.SetActive(true);
+            interacting = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Interactable"))
+        {
+            interact.SetActive(false);
+            interacting = false;
+        }
+    }
+}
