ca03521605b56d036c83b71b43ebe8809a79ba77
Blake <blakespyda@hotmail.com>
2022-01-17 15:20:27 +0100

    Implemented better Puzzle logic
    Still need to make a better way to reset the puzzle, but first time around working as intended

---
+++ Crystal Sword/Assets/Scripts/Shit That just works/StartPuzzle.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPuzzle : MonoBehaviour
+{
+    private bool ready;
+    public bool go;
+
+
+    private void Awake()
+    {
+        ready = false;
+        go = false;
+    }
+
+    private void Update()
+    {
+        if(ready && Input.GetKeyDown(KeyCode.R))
+        {
+            go = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ready = true;
+        }        
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ready = false;
+        }
+    }
+
+    private void Reset()
+    {
+        ready = false;
+        go = false;
+    }
+
+    private void OnEnable()
+    {
+        PuzzleManager.Resetting += Reset;
+    }
+
+    private void OnDisable()
+    {
+        PuzzleManager.Resetting -= Reset;
+    }
+}
