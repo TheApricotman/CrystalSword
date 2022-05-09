232258b4f168ff6fe5a561543543f91251ab504f
Blake <blakespyda@hotmail.com>
2022-05-09 15:28:33 +1000

    Started SaveLoad system
    based on interface, interface talks to saveable entity script which talks to saveload object. will get it actually working next time

---
+++ Crystal Sword/Assets/Scripts/Game management/SaveableEntity.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveableEntity : MonoBehaviour
+{
+    [SerializeField]
+    private string id;
+    public string ID => id;
+
+    [ContextMenu("Generate ID")]
+    private void GenerateID()
+    {
+        id = Guid.NewGuid().ToString();
+    }
+
+    //Find all Isaveable objects
+    public object SaveState()
+    {
+        var state = new Dictionary<string, object>();
+        foreach (var saveable in GetComponents<ISaveable>())
+        {
+            state[saveable.GetType().ToString()] = saveable.SaveState();
+        }
+        return state;
+    }
+    public void LoadState(object state)
+    {
+        var stateDictionary = (Dictionary<string, object>)state;
+        foreach (var saveable in GetComponents<ISaveable>())
+        {
+            string typeName = saveable.GetType().ToString();
+            if(stateDictionary.TryGetValue(typeName, out object savedState))
+            {
+                saveable.LoadState(savedState);
+            }
+        }
+    }
+}
