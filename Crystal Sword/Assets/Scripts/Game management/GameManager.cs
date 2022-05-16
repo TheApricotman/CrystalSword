d68f1f62de80f93cc81aab61a2440f688bd5304e
Blake <blakespyda@hotmail.com>
2022-05-16 18:08:59 +1000

    Save system still not working but maybe i added to it?
    who knows

---
+++ Crystal Sword/Assets/Scripts/Game management/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour, ISaveable  
+{
+    private int levelNumber;
+
+    private void Start()
+    {
+        levelNumber = SceneManager.GetActiveScene().buildIndex;
+        if(levelNumber < 1)
+        {
+            levelNumber++;
+        }
+    }
+
+    public object SaveState()
+    {
+        return new SaveData
+        {
+            levelNumber = levelNumber
+        };
+    }
+
+    public void LoadState(object state)
+    {
+        var saveData = (SaveData)state;
+        saveData.levelNumber = levelNumber;
+        SceneManager.LoadScene(levelNumber);
+    }
+
+    [Serializable]
+    private struct SaveData
+    {
+        public int levelNumber;
+    }
+}
