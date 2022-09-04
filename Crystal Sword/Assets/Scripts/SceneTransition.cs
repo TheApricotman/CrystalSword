4c7dccf71f0e625828934cb3ead8e9b09a428641
Blake <blakespyda@hotmail.com>
2022-09-04 16:16:49 +1000

    BGM added
    all level tracks are in and fade in and out between levels

--- Crystal Sword/Assets/Scripts/SceneTransition.cs
+++ Crystal Sword/Assets/Scripts/SceneTransition.cs
@@ -8,16 +8,27 @@ public class SceneTransition : MonoBehaviour
     [SerializeField]
     private string sceneToLoad;
     [SerializeField]
+    //private AudioClip songToPlay;
+    private AudioSource audioSource;
+    [SerializeField]
     private Vector2 playerPos;
     [SerializeField]
     private VectorValue playerStorage;
     [SerializeField]
     private Animator transition;
+    [SerializeField]
+    private Animator musicTransition;
+
+    private void Start()
+    {
+        musicTransition = Camera.main.GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")&& !other.isTrigger)
         {
+            audioSource = Camera.main.GetComponent<AudioSource>();
             playerStorage.intialValue = playerPos;
             StartCoroutine(LoadLevel());
         }
@@ -26,7 +37,9 @@ public class SceneTransition : MonoBehaviour
     IEnumerator LoadLevel()
     {
         transition.SetTrigger("StartFade");
+        musicTransition.SetTrigger("StartFade");
         yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene(sceneToLoad);
+       
     }
 }
